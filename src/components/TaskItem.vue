f199dc2923c96286b21966688617c4bb5347192e
Francisco Seijas <panchoseijas@gmail.com>
2024-09-18 23:30:47 -0300

    estilos

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class="item">
-    <div class="item-name">
-      <span :class="{ completed: task.completed }">{{ task.text }}</span>
+  <div class="flex flex-wrap justify-between content-center p-1 w-[500px]">
+    <div class="flex justify-end w-1/6">
+      <span class="py-3 px-0">
+        {{ task.id }}
+      </span>
     </div>
-    <div>
-      <button @click="onToggleTaskCompleted">Toggle</button>
+    <div class="flex justify-between p-3 px-0 content-center w-3/6">
+      <span
+        :class="{
+          completed: task.completed,
+        }"
+        >{{ task.text }}</span
+      >
     </div>
-    <div><button @click="handleRemoveTask">Remove</button></div>
+    <div class="flex gap-2">
+      <button
+        :class="{
+          'bg-green-100 text-green-800 border-green-500': task.completed,
+          'bg-gray-100 text-gray-800 border-gray-500': !task.completed,
+          'p-3 rounded-lg': true, // Always apply these classes
+        }"
+        @click="onToggleTaskCompleted"
+      >
+        Toggle
+      </button>
+      <button
+        class="bg-red-100 text-red-800 bordr-red-500 p-1 rounded-xl"
+        @click="handleRemoveTask"
+      >
+        X
+      </button>
+    </div>
   </div>
 </template>
 
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,30 +1,45 @@
 <template>
-  <div class="wrapper">
-    <div>
+  <div class="flex justify-center items-center flex-col p-3 gap-5 w-full">
+    <div class="flex gap-2">
       <input
+        class="border-2 border-gray-300 px-4 py-2 rounded-lg"
         v-model="newTask"
         @keyup.enter="handleAddTask"
         placeholder="Add a new task"
       />
-      <button @click="handleAddTask">Add Task</button>
+      <button
+        class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+        @click="handleAddTask"
+      >
+        Add Task
+      </button>
+      <button @click="toggleCompleted">Show completed</button>
     </div>
-    <ul class="lista">
+    <ul v-if="!showCompleted" class="flex flex-col justify-start">
       <li v-for="task in tasks" :key="task.id">
+        <TaskItem :task="task" />
+      </li>
+    </ul>
+
+    <ul v-if="showCompleted" class="flex flex-col justify-start">
+      <li v-for="task in completedTasks" :key="task.id">
         <TaskItem :task="task" />
       </li>
     </ul>
   </div>
 </template>
 
-<script>
+<!-- <script>
 import { mapGetters, mapActions } from "vuex"
 import TaskItem from "./TaskItem.vue"
+let id = 1
 
 export default {
   components: { TaskItem },
   data() {
     return {
       newTask: "",
+      showCompleted: false,
     }
   },
   computed: {
@@ -35,7 +50,7 @@ export default {
     handleAddTask() {
       if (this.newTask.trim()) {
         const task = {
-          id: Date.now(),
+          id: id++,
           text: this.newTask,
           completed: false,
         }
@@ -43,6 +58,58 @@ export default {
         this.newTask = ""
       }
     },
+    toggleCompleted() {
+      this.showCompleted = !this.showCompleted
+    },
+  },
+}
+</script> -->
+
+<script>
+import { ref, computed } from "vue"
+import { useStore } from "vuex"
+import TaskItem from "./TaskItem.vue"
+
+let id = 1
+
+export default {
+  components: { TaskItem },
+
+  setup() {
+    const newTask = ref("")
+    const showCompleted = ref(false)
+
+    const store = useStore()
+
+    const tasks = computed(() => store.getters.tasks)
+    const completedTasks = computed(() =>
+      [...store.getters.tasks].filter((task) => task.completed)
+    )
+
+    const handleAddTask = () => {
+      if (newTask.value.trim()) {
+        const task = {
+          id: id++,
+          text: newTask.value,
+          completed: false,
+        }
+        store.dispatch("addTask", task)
+        newTask.value = ""
+      }
+    }
+
+    const toggleCompleted = () => {
+      showCompleted.value = !showCompleted.value
+    }
+
+    return {
+      newTask,
+      showCompleted,
+      tasks,
+      handleAddTask,
+      toggleCompleted,
+      completedTasks,
+    }
   },
 }
 </script>
